1ecec25157952c759f33405764f840eaa9708330
Rituraj Mahato <94545894+Rituraj-M50@users.noreply.github.com>
2022-01-01 23:25:37 +0530

    Update main-music.js

--- main-music.js
+++ main-music.js
@@ -1,14 +1,3 @@
-// To add more song, just copy the following code and paste inside the array
-
-//   {
-//     name: "Here is the music name",
-//     artist: "Here is the artist name",
-//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
-//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
-//   }
-
-//paste it inside the array as more as you want music then you don't need to do any other thing
-
 let allMusic = [
     {
       name: "Master the Blaster",
@@ -16,66 +5,6 @@ let allMusic = [
       img: "master_the_blaster",
       src: "master_the_blaster"
     },
-    /*{
-      name: "Dynamite",
-      artist: "BTS",
-      img: "dynamite",
-      src: "dynamite"
-    },
-    {
-      name: "Love Nwantiti",
-      artist: "Ckay",
-      img: "love_nwantiti",
-      src: "love_nwantiti"
-    },
-    {
-      name: "Crazy Train",
-      artist: "Ozzy Osbourne",
-      img: "crazy_train",
-      src: "crazy_train"
-    },
-    {
-      name: "Bohemian Rhapsody",
-      artist: "Queen",
-      img: "bohemian_rhapsody",
-      src: "bohemian_rhapsody"
-    },
-    {
-      name: "Layla",
-      artist: "Derek & The Dominos",
-      img: "layla",
-      src: "layla"
-    },
-    {
-      name: "Stairway to Heaven",
-      artist: "Led Zeppelin",
-      img: "stairway_heaven",
-      src: "stairway_heaven"
-    },
-    {
-      name: "Shape of You",
-      artist: "Ed Sheeran",
-      img: "shape_of_you",
-      src: "shape_of_you"
-    },
-    {
-      name: "Counting Stars",
-      artist: "One Republic",
-      img: "counting_stars",
-      src: "counting_stars"
-    },
-    {
-      name: "On My Way",
-      artist: "Alan Walker",
-      img: "on_my_way",
-      src: "on_my_way"
-    },
-    {
-      name: "Échame La Culpa",
-      artist: "Luis Fonsi",
-      img: "la_culpa",
-      src: "la_culpa"
-    }, */
     {
       name: "Tum Hi Ho",
       artist: "Arijit Singh",
@@ -87,12 +16,5 @@ let allMusic = [
       artist: "Shawn Mendes, Camila Cabello",
       img: "senorita",
       src: "senorita"
-    },
-    // like this paste it and remember to give comma after ending of this bracket }
-    // {
-    //   name: "Here is the music name",
-    //   artist: "Here is the artist name",
-    //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
-    //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
-    // }
+    }
   ];
